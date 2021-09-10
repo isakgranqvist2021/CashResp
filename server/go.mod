16cecf17d29f508a784793074cef1c7bf751b059
isakgranqvist2021 <isakwebdev@gmail.com>
2021-09-10 02:39:26 +0200

    qwt

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module github.com/isakgranqvist2021/cashresp
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.18.0 // indirect
+require (
+	github.com/gofiber/fiber/v2 v2.18.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
---
+++ server/utils/database.go
@@ -0,0 +1,58 @@
+package database
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func Connect() *sql.DB {
+	// read .env file in root directory
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	URI := fmt.Sprintf("%s:%s@/%s",
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE"))
+
+	db, err := sql.Open("mysql", URI)
+
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
+}
+
+func GetKeys() (map[string]map[string]string, error) {
+	bytes, err := ioutil.ReadFile("./store/keys.json")
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("internal server error")
+	}
+
+	data := make(map[string]map[string]string)
+	if err := json.Unmarshal(bytes, &data); err != nil {
+		fmt.Println(err)
+		return nil, errors.New("internal server error")
+	}
+
+	return data, nil
+}
